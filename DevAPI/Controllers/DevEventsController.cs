f22d5293fbce4084503809567bbb691ba109e37e
LinsDeveloper <pedrolins42@gmail.com>
2023-08-26 16:42:05 -0300

    Adicionar arquivos de projeto.

---
+++ DevAPI/Controllers/DevEventsController.cs
@@ -0,0 +1,82 @@
+﻿using DevAPI.Entities;
+using DevAPI.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DevAPI.Controllers
+{
+    [Route("api/dev-events")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+        private readonly DevEventsDbContext _context;
+
+
+        public DevEventsController(DevEventsDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var devEvents = _context.DevEvents.Where(d => !d.IsDeleted).ToList();
+
+            return Ok(devEvents);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(devEvent);
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Post(Guid id,DevEvents devEvents)
+        {
+            _context.DevEvents.Add(devEvents);
+
+            return CreatedAtAction(nameof(GetById), new { id = devEvents.Id }, devEvents);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id,DevEvents input)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Update(input.Title, input.Description, input.StartDate, input.EndDate);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Delete();
+
+            return NoContent();
+
+        }
+    }
+}
---
+++ DevAPI/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DevAPI.Entities;
+
+namespace DevAPI.Persistence
+{
+    public class DevEventsDbContext
+    {
+        public List<DevEvents> DevEvents { get; set; }
+
+        public DevEventsDbContext()
+        {
+            DevEvents = new List<DevEvents>();  
+        }
+    }
+}
